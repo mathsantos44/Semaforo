3e0bdc3faf87308ea96aeb280529a0d390546e9a
Matheus <math.olsantos@gmail.com>
2021-05-25 11:49:13 -0300

    Primeira Alteração

--- parte1/parte1.ino
+++ parte1/parte1.ino
@@ -13,16 +13,42 @@
  * Não poderá ser utilizado o comando Delay durante a troca de sinalizações pra não ocorrer erros e o usuário não ser reconhecido
  */
 
-#define sem1verm 2;
-#define sem1amar 3;
-#define sem1verd 4;
+#define sem1verm 2
+#define sem1amar 3
+#define sem1verd 4
+
+unsigned long delay1 = 0;
+unsigned long delay2 = 0;
+unsigned long delay3 = 0;
+
+const int tempoVerde = 5000;
+const int tempoAmar = 2000;
+
+int fase;
 
 void setup() {
   // put your setup code here, to run once:
-    
+  Serial.begin(9600);
+  for (int i = 2; i <= 4; i++) {
+    pinMode(i, OUTPUT);
+    Serial.println(i);
+
+    delay1 = millis();
+
+    fase = 1;
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (fase == 1) {
+    delay1 = millis();
+    digitalWrite(sem1verm, LOW);
+    digitalWrite(sem1amar, LOW);
+    digitalWrite(sem1verd, HIGH);
+    
+    if ((millis() - dalay1) > tempoVerde) {
+      fase = 2;
+    }
+  }
 }
