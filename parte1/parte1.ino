92ee68597f40f80c71c71d442aa3b384382e5be7
Matheus <math.olsantos@gmail.com>
2021-05-25 10:04:23 -0300

    Parte 1 do Projeto

---
+++ parte1/parte1.ino
@@ -0,0 +1,28 @@
+/*
+ * Autor: Matheus Oliveira dos Santos
+ * Data: 20/05/2021
+ * Projeto: Semáforo
+ */
+
+/* Algoritmo
+ * Enquanto 1 semáforo estiver aberto, todos os outros tem que estar fechados pra não ocorrer colisão
+ * O semáforo de pedestre só poderá ser aberto após o funcionamento de todos
+ * Cada semáforo ficará 5 segundo no verde e 3 no amarelo
+ * O semáforo de pedestre poderá ser ativo somente quando o botão for ativo, caso contrario, ele não será aberto
+ * Essa parte somente farei o funcionamento de um semáforo
+ * Não poderá ser utilizado o comando Delay durante a troca de sinalizações pra não ocorrer erros e o usuário não ser reconhecido
+ */
+
+#define sem1verm 2;
+#define sem1amar 3;
+#define sem1verd 4;
+
+void setup() {
+  // put your setup code here, to run once:
+    
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
